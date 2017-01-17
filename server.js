5a8cbcf58798439160b7a5f0d0efa06e8f0cd9e9
Mark Brown II <mbrownii@doc.gov>
2017-01-17 17:05:24 -0500

    Adds notes to server.js file
    Gives context for the way of setting up the server in NodeJS

--- server.js
+++ server.js
@@ -6,24 +6,50 @@ const cred = require('./cred/credentials.js');
 const exphbs = require('express-handlebars');
 const highlighter = require('keyword-highlighter');
 
+// signs into the database
 var url = 'mongodb://' + cred.username + ':' + cred.password + cred.datab;
+
 var db;
+
+//sets up 'router' Express router for this application
 var router = express.Router();
+
+//sets up 'adminRouter' Express router for this application
 var adminRouter = express.Router();
 
+// registers filename 'handlebars', and calls the exphb variable from above
+// and passes in the parmeters when it get a file with the extention
 app.engine('handlebars', exphbs({
 	layoutsDir: './prod/views/layouts/',
 	defaultLayout: 'main'
 }));
 
+// sets defaul engine to 'handlebars' which then triggers the app.engine line
 app.set('view engine', 'handlebars');
+
+// sets directory for application's views
 app.set('views', process.cwd() + '/prod/views/');
+
+// sets the port value 
+// process.env is the environment variable which can change depending 
+// on if it is on Heroku or on your local computer
 app.set('port', process.env.PORT || 3000);
+
+// serves static conent for the app from the public directory
 app.use(express.static(__dirname + '/prod/public'));
+
+// parses application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: true}));
+
+// parses application/json
 app.use(bodyParser.json());
+
+// when you get / use the router registered above
 app.use('/', router);
+
+// when you get /admin use the AdminRouter registered above
 app.use('/admin', adminRouter);
+
 
 MongoClient.connect(url, (err,database) => {
 	if (err) return console.log(err);
@@ -165,6 +191,7 @@ MongoClient.connect(url, (err,database) => {
 
 })
 
+// start app after getting the 'port' variable
 app.listen(app.get('port'));
 
 
