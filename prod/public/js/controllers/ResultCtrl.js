5e9426b3f16f46805247c0145dda75b23ea2f4f1
Mark Brown II <mbrownii@doc.gov>
2017-02-01 10:52:43 -0500

    Adds notes to ResultCtrl to give developers some insight

--- prod/public/js/controllers/ResultCtrl.js
+++ prod/public/js/controllers/ResultCtrl.js
@@ -1,32 +1,32 @@
 angular.module('ResultCtrl', []).controller('ResultController', function($http,$scope, $location, ResultCall) {
 
-	//getInformation = function(term) {
-	//	console.log('this is searchTerm ' + term);
+	$scope.getInformation = function (term) {
 		
 		//getLyrics is defined as a property of the factory 'ResultCall' defined in ResultService.js - returns a promise
 		//because ResultCall is an object we have to use dot notation
-	//}
-
-	$scope.getInformation = function (term) {
+		// calling the function using the 'term' passed in from the view ng-submit="getInformation(searchTerm)"
 		ResultCall.getLyrics(term)
-			// data is the information returned from the API call
-			// then() is run only if the promise is kept
+
+			// response is the information returned from the function getLyrics()'s line 'return response' after then()
+			
+			// then() is run only if the promise is kept as written in the getLyrics() method
 			.then(function(response) {
 				
-
+				// data validation to make sure that the information being returned is an object
 				if (typeof response === "object") {
 
-					
+					// because of closure you need to use $scope.$parent to get the data passed up
+					// to then be used on the view that you would like and accessible on the 
+					// controller
+					// http://stackoverflow.com/questions/21453697/angularjs-access-parent-scope-from-child-controller
 					$scope.$parent.songs = response.data;
-					$scope.$parent.tagline = "Hello";
+
+					// this redirects the app without reloading the page
 					$location.path('/results');
-					console.log('it is getting to typeof');
 				}
 				else {
 					return error = "There was an error!";
 				}
-
-
 			})
 			.catch(function(error) {
 				console.log('The erorr is ' + error);
