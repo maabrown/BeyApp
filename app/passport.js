d27564ad4a27f63853e16d9e07bdacaa04a3d566
Mark Brown II <mbrownii@doc.gov>
2017-04-07 20:02:52 -0400

    Changes path to User.js

--- app/passport.js
+++ app/passport.js
@@ -1,5 +1,5 @@
 const LocalStrategy = require('passport-local').Strategy;
-const User = require('./app/models/user');
+const User = require('./models/user');
 const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 
 // loads in Google+ authentication
