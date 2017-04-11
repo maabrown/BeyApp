77f4fec93f9cc794ec343c78be71273b75184987
Mark Brown II <mbrownii@doc.gov>
2017-04-11 17:12:53 -0400

    Begins Gulp file project
    Adds jshint and jscrc to file to do JS linting

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,17 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass');
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var jscs = require('gulp-jscs');
+
+gulp.task('vet', function() {
+	return gulp
+		.src([
+			'./server.js'
+			// './app/**/*.js',
+			// './cred/**/*.js',
+			// './prod/public/**/*.js'
+		])
+		.pipe(jscs())
+		.pipe(jscs.reporter())
+		.pipe(jshint())
+		.pipe(jshint.reporter('jshint-stylish', {verbose: true}))
+})
